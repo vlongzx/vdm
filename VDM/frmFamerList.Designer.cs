7993452d218f047d454b7ad09a53f9df090eddf0
Reji <2193208365@qq.com>
2023-08-26 13:43:11 +0800

    测试

--- VDM/frmFamerList.Designer.cs
+++ VDM/frmFamerList.Designer.cs
@@ -28,19 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.uiTextBox1 = new Sunny.UI.UITextBox();
+            this.uiDatePicker1 = new Sunny.UI.UIDatePicker();
+            this.uiLabel2 = new Sunny.UI.UILabel();
             this.SuspendLayout();
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.AutoSize = true;
+            this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.uiLabel1.Location = new System.Drawing.Point(294, 43);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(82, 41);
+            this.uiLabel1.TabIndex = 0;
+            this.uiLabel1.Text = "姓名";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // uiTextBox1
+            // 
+            this.uiTextBox1.ButtonSymbolOffset = new System.Drawing.Point(0, 0);
+            this.uiTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox1.Location = new System.Drawing.Point(383, 33);
+            this.uiTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox1.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uiTextBox1.Name = "uiTextBox1";
+            this.uiTextBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.uiTextBox1.ShowText = false;
+            this.uiTextBox1.Size = new System.Drawing.Size(186, 51);
+            this.uiTextBox1.TabIndex = 1;
+            this.uiTextBox1.Text = "uiTextBox1";
+            this.uiTextBox1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox1.Watermark = "";
+            // 
+            // uiDatePicker1
+            // 
+            this.uiDatePicker1.DropDownStyle = Sunny.UI.UIDropDownStyle.DropDownList;
+            this.uiDatePicker1.FillColor = System.Drawing.Color.White;
+            this.uiDatePicker1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.uiDatePicker1.Location = new System.Drawing.Point(383, 167);
+            this.uiDatePicker1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiDatePicker1.MaxLength = 10;
+            this.uiDatePicker1.MinimumSize = new System.Drawing.Size(63, 0);
+            this.uiDatePicker1.Name = "uiDatePicker1";
+            this.uiDatePicker1.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
+            this.uiDatePicker1.Size = new System.Drawing.Size(170, 29);
+            this.uiDatePicker1.SymbolDropDown = 61555;
+            this.uiDatePicker1.SymbolNormal = 61555;
+            this.uiDatePicker1.TabIndex = 1;
+            this.uiDatePicker1.Text = "2020-05-08";
+            this.uiDatePicker1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiDatePicker1.Value = new System.DateTime(2020, 5, 8, 0, 0, 0, 0);
+            this.uiDatePicker1.Watermark = "";
+            // 
+            // uiLabel2
+            // 
+            this.uiLabel2.AutoSize = true;
+            this.uiLabel2.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.uiLabel2.Location = new System.Drawing.Point(294, 155);
+            this.uiLabel2.Name = "uiLabel2";
+            this.uiLabel2.Size = new System.Drawing.Size(82, 41);
+            this.uiLabel2.TabIndex = 2;
+            this.uiLabel2.Text = "日期";
+            this.uiLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // frmFamerList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.uiLabel2);
+            this.Controls.Add(this.uiDatePicker1);
+            this.Controls.Add(this.uiTextBox1);
+            this.Controls.Add(this.uiLabel1);
             this.Name = "frmFamerList";
             this.Text = "frmFamerList";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Sunny.UI.UILabel uiLabel1;
+        private Sunny.UI.UITextBox uiTextBox1;
+        private Sunny.UI.UIDatePicker uiDatePicker1;
+        private Sunny.UI.UILabel uiLabel2;
     }
 }
