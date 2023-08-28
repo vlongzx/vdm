e742e07ac8733036a3cb59da87124aa3e8b3e946
vlongzx <75635807@qq.com>
2023-08-28 08:38:15 +0800

    增加了对字典项的维护

--- BLL/DictBLL.cs
+++ BLL/DictBLL.cs
@@ -1,5 +1,6 @@
 ﻿using com.vdm.common;
 using com.vdm.dal;
+using com.vdm.model;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -28,18 +29,45 @@ namespace com.vdm.bll
         {
             DataTable dtDict = this.dictDAL.getDict(dict_code);
             List<KeyValue> list = new List<KeyValue>();
-            if(dtDict != null )
+            if (dtDict != null)
             {
                 KeyValue kv = null;
-                foreach(DataRow row in dtDict.Rows)
+                foreach (DataRow row in dtDict.Rows)
                 {
                     string key = row["key"].ToString();
                     string value = row["value"].ToString();
-                    kv = new KeyValue(key,value);
+                    kv = new KeyValue(key, value);
                     list.Add(kv);
                 }
             }
             return list;
+        }
+        /// <summary>
+        ///  获得不重复的字典项
+        /// </summary>
+        /// <returns></returns>
+        public DataTable getDistinctDict()
+        {
+            return this.dictDAL.getDistinctDict();
+        }
+
+        /// <summary>
+        ///  获得所有字典项
+        /// </summary>
+        /// <returns></returns>
+        public DataTable getAllDict()
+        {
+            return this.dictDAL.getAllDict();
+        }
+
+        public DataTable getDictByCode(string dict_code)
+        {
+            return this.dictDAL.getDictByCode(dict_code);
+        }
+
+        public Result addDict(Dict dict)
+        {
+            return this.dictDAL.CreateDict(dict);
         }
     }
 }
