f02de6a94aa8b1a447eb1793dd05048f480e5561
vlongzx <75635807@qq.com>
2023-09-04 22:15:21 +0800

    修改缺陷

--- VDM/Program.cs
+++ VDM/Program.cs
@@ -18,7 +18,7 @@ namespace com.vdm.form
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            if(File.Exists(System.IO.Directory.GetCurrentDirectory() + "\\db_file\\vdm_data.db;") == false)
+            if(File.Exists(System.IO.Directory.GetCurrentDirectory() + "\\db_file\\vdm_data.db") == false)
             {
                 MessageBox.Show("数据库文件不存在或路径不正确，请检查。", "运行错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
