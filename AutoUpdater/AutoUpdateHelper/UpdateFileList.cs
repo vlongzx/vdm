886bed0f577d0b17bc646b8e6e6258df987e974b
vlongzx <75635807@qq.com>
2023-09-09 19:35:42 +0800

    增加自动更新程序

---
+++ AutoUpdater/AutoUpdateHelper/UpdateFileList.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.vdm.AutoUpdater
+{
+    public class UpdateFileList : List<LocalFile>
+    {
+
+    }
+}
--- VDM/frmLogin.cs
+++ VDM/frmLogin.cs
@@ -1,4 +1,5 @@
-﻿using com.vdm.bll;
+﻿using com.vdm.AutoUpdater;
+using com.vdm.bll;
 using com.vdm.common;
 using Sunny.UI;
 using System;
@@ -8,9 +9,11 @@ using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace com.vdm.form
 {
@@ -63,10 +66,61 @@ namespace com.vdm.form
         {
             this.labSystemName.Text = ConfigurationManager.AppSettings["system_name"];
         }
-
-        private void uiLabel3_Click(object sender, EventArgs e)
+        /// <summary>
+        ///  检查程序更新
+        /// </summary>
+        public void InitCheckUpdate()
         {
-
+            #region check and download new version program
+            bool bHasError = false;
+            IAutoUpdater autoUpdater = new com.vdm.AutoUpdater.AutoUpdater();
+            try
+            {
+                autoUpdater.Update();
+            }
+            catch (WebException exp)
+            {
+                MessageBox.Show("服务器连接失败");
+                bHasError = true;
+            }
+            catch (XmlException exp)
+            {
+                bHasError = true;
+                MessageBox.Show("下载更新文件错误");
+            }
+            catch (NotSupportedException exp)
+            {
+                bHasError = true;
+                MessageBox.Show("升级文件配置错误");
+            }
+            catch (ArgumentException exp)
+            {
+                bHasError = true;
+                MessageBox.Show("下载升级文件错误");
+            }
+            catch (Exception exp)
+            {
+                bHasError = true;
+                MessageBox.Show("更新过程中出现错误");
+            }
+            finally
+            {
+                if (bHasError == true)
+                {
+                    try
+                    {
+                        autoUpdater.RollBack();
+                    }
+                    catch (Exception)
+                    {
+                        //Log the message to your file or database
+                    }
+                }
+                OperProcess op = new OperProcess();
+                //启动进程
+                op.StartProcess();
+            }
+            #endregion
         }
     }
 }
