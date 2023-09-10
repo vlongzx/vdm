24a0851792a4280aa45609e69d958d8607c2f02a
vlongzx <75635807@qq.com>
2023-09-10 15:50:51 +0800

    增加了对异常的处理。

--- AutoUpdater/Program.cs
+++ AutoUpdater/Program.cs
@@ -27,28 +27,28 @@ namespace com.vdm.AutoUpdater
             }
             catch (WebException exp)
             {
-                MessageBox.Show("服务器连接失败");
+                MessageBox.Show("服务器连接失败。错误消息"+exp.Message);
                 bHasError = true;
             }
             catch (XmlException exp)
             {
                 bHasError = true;
-                MessageBox.Show("下载更新文件错误");
+                MessageBox.Show("下载更新文件错误。错误消息"+exp.Message);
             }
             catch (NotSupportedException exp)
             {
                 bHasError = true;
-                MessageBox.Show("升级文件配置错误");
+                MessageBox.Show("升级文件配置错误。错误消息" + exp.Message);
             }
             catch (ArgumentException exp)
             {
                 bHasError = true;
-                MessageBox.Show("下载升级文件错误");
+                MessageBox.Show("下载升级文件错误。错误消息" + exp.Message);
             }
             catch (Exception exp)
             {
                 bHasError = true;
-                MessageBox.Show("更新过程中出现错误");
+                MessageBox.Show("更新过程中出现错误。错误消息" + exp.Message);
             }
             finally
             {
@@ -58,9 +58,9 @@ namespace com.vdm.AutoUpdater
                     {
                         autoUpdater.RollBack();
                     }
-                    catch (Exception)
+                    catch (Exception exp)
                     {
-                        //Log the message to your file or database
+                        MessageBox.Show("更新回滚过程中出现错误。错误消息" + exp.Message);
                     }
                 }
                 OperProcess op = new OperProcess();
