19a8364e45bebd60ca889d3554efaf0b879bd82c
Reji <2193208365@qq.com>
2023-09-14 17:08:41 +0800

    优化buf：导出数据的文件名后面的数字改为当前日期+时间（时分）

--- VDM/frmExportExcel.cs
+++ VDM/frmExportExcel.cs
@@ -109,7 +109,7 @@ namespace com.vdm.form
             if (result == DialogResult.OK)
             {
                 string selectedPath = this.folderBrowserDialog.SelectedPath;
-                string fileName = object_name + "信息导出_"+ DateTime.Now.ToString("yyyymmdd") +".xlsx";
+                string fileName = object_name + "信息导出_"+ DateTime.Now.ToString("yyyyMMddHHmm") +".xlsx";
                 this.tbSavePath.Text = Path.Combine(selectedPath, fileName);
             }
         }
